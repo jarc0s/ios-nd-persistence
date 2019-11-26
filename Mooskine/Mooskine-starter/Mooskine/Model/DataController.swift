b17bbc8e8986f68f293550c2656384a2767b46b6
juan <juan.arcos.hdz@gmail.com>
2019-11-25 23:09:01 -0600

    Feature add coredata data controller

---
+++ Mooskine/Mooskine-starter/Mooskine/Model/DataController.swift
@@ -0,0 +1,32 @@
+  //
+//  DataController.swift
+//  Mooskine
+//
+//  Created by juan on 11/25/19.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import Foundation
+import CoreData
+  
+class DataController {
+    
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion:(() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores{ storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            completion?()
+        }
+    }
+}
